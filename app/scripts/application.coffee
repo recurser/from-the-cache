8a2017bbe3b937a0bc66623e656ef1dd16db8991
Dave Perrett <dave@recurser.com>
2010-11-02 22:01:00 +1100

    Updated jscript cache tag.

--- app/scripts/application.coffee
+++ app/scripts/application.coffee
@@ -19,20 +19,28 @@ app: {
         link_text = 'the original'
         
       # Position and style the tag.
-      tag = $('<div>From <a href="'+scrape_source+'">'+link_text+'</a></div>')
+      tag = $('<div>From <a id="source_link" href="'+scrape_source+'">'+link_text+'</a><a id="home_link" href="http://fromthecache.com/">fromthecache.com</a></div>')
       tag.css 'position',         'absolute'
       tag.css 'top',              '5px'
       tag.css 'right',            '5px'
       tag.css 'background-color', '#AA0101'
       tag.css 'color',            '#fff'
       tag.css 'padding',          '5px'
-      tag.css 'font-size',        '15px'
+      tag.css 'font-size',        '16px'
       tag.css 'font-weight',      'bold'
-      link = tag.find 'a'
+      link = tag.find 'a#source_link'
       link.css 'color',           '#fff'
       link.css 'text-decoration', 'underline'
-      link.css 'font-size',       '15px'
+      link.css 'font-size',       '16px'
       link.css 'font-weight',     'bold'
+      link = tag.find 'a#home_link'
+      link.css 'color',           '#fff'
+      link.css 'text-decoration', 'none'
+      link.css 'font-size',       '8px'
+      link.css 'font-weight',     'normal'
+      link.css 'display',         'block'
+      link.css 'margin',          '0 auto'
+      link.css 'text-align',      'center'
       $('body').append tag
     
   handle_search: ->
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -17,20 +17,28 @@
       if ($('base').length > 0) {
         scrape_source = unescape($('input#scrape_source').attr('value'));
         scrape_source.indexOf('webcache.googleusercontent.com') > 0 ? (link_text = 'the cache') : (link_text = 'the original');
-        tag = $('<div>From <a href="' + scrape_source + '">' + link_text + '</a></div>');
+        tag = $('<div>From <a id="source_link" href="' + scrape_source + '">' + link_text + '</a><a id="home_link" href="http://fromthecache.com/">fromthecache.com</a></div>');
         tag.css('position', 'absolute');
         tag.css('top', '5px');
         tag.css('right', '5px');
         tag.css('background-color', '#AA0101');
         tag.css('color', '#fff');
         tag.css('padding', '5px');
-        tag.css('font-size', '15px');
+        tag.css('font-size', '16px');
         tag.css('font-weight', 'bold');
-        link = tag.find('a');
+        link = tag.find('a#source_link');
         link.css('color', '#fff');
         link.css('text-decoration', 'underline');
-        link.css('font-size', '15px');
+        link.css('font-size', '16px');
         link.css('font-weight', 'bold');
+        link = tag.find('a#home_link');
+        link.css('color', '#fff');
+        link.css('text-decoration', 'none');
+        link.css('font-size', '8px');
+        link.css('font-weight', 'normal');
+        link.css('display', 'block');
+        link.css('margin', '0 auto');
+        link.css('text-align', 'center');
         return $('body').append(tag);
       }
     },
